e375a0f7d477adb9e27ba115b9d3a53c59df10ef
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-08-31 21:19:38 -0300

    Changed the app's name to iNails and added Nodemailer dependency.

--- src/config/database.js
+++ src/config/database.js
@@ -4,12 +4,12 @@ module.exports = {
     host: '192.168.99.100',
     username: 'postgres',
     password: 'docker',
-    database: 'gobarber',
+    database: 'iNails',
     define: {
       timestamps: true,
       underscored: true,
       underscoredAll: true,
     },
   },
-  mongoConfig: 'mongodb://192.168.99.100:27017/gobarber',
+  mongoConfig: 'mongodb://192.168.99.100:27017/iNails',
 };
