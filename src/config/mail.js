c9196bc9075c6ab5e35e801c51e0e452aacbd333
Frederiko C. M. Ribeiro <frederiko.cmr@gmail.com>
2019-08-31 22:28:02 -0300

    Added email for cancelation with nodemailer

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,1 +1,12 @@
-export default {};
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'ba8d7fb374d771',
+    pass: '2708b9b27c470b',
+  },
+  default: {
+    from: 'Equipe iNails <noreply@inails.com>',
+  },
+};
